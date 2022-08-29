c71bc5fab3b6f726c6edb4a89620f1778a026e5c
chamovski <74669727+chamovski@users.noreply.github.com>
2022-08-29 20:28:19 +0200

    Add files via upload

---
+++ HomeworkCSharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3, num4;
+
+            int result;
+
+            Console.WriteLine("Enter Number 1");
+
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Number 2");
+
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Number 3");
+
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Number 4");
+
+            num4 = Convert.ToInt32(Console.ReadLine());
+
+            result = num1 * num2 * num3 * num4;
+
+            double res = Convert.ToDouble(result);
+
+            Console.WriteLine("Your Result is  " + res);
+
+        }
+    }
+}
