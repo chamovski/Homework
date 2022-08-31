89b4dab5c62ffd5cdb5f91ebbcceb82a575b8312
chamovski <74669727+chamovski@users.noreply.github.com>
2022-08-31 15:28:19 +0200

    Homework

---
+++ Homework2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //wrong
+            //int i;
+            //for (i = 1; i < 10; i++)
+            //{
+            //    if ((i % 2) = 0)
+            //        continue;
+            //    Console.WriteLine(i);
+            //}
+
+            //correct
+            int i;
+            for (i = 1; i < 10; i++)
+            {
+                if ((i % 2) == 0) //se koristeshe operator za dodeluvanje na vrednost a treba da se koristi operator za sporedba
+                    continue;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
