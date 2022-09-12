7872431d8a6dd792f3d15dfc21dfeff3be8a6a0e
chamovski <74669727+chamovski@users.noreply.github.com>
2022-09-12 20:12:04 +0200

    Homework Operants

---
+++ Homework_Operants/HwEx01/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter first number:");
+var num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number:");
+var num2 = Convert.ToInt32(Console.ReadLine());
+
+var res = num1 + num2;
+Console.WriteLine($"The sum of {num1} and {num2} is {res}.");
---
+++ Homework_Operants/HwEx02/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter first number:");
+var num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number:");
+var num2 = Convert.ToInt32(Console.ReadLine());
+
+var res = num1 / num2;
+Console.WriteLine($"{num1} divided by {num2} is {res}.");
