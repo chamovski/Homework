aaf9a952ba1aae8ae2e3942d594dcd7c3492acf8
chamovski <74669727+chamovski@users.noreply.github.com>
2022-09-12 20:18:13 +0200

    Homework Operants

---
+++ Homework_Operants/HwEx12/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter a number: ");
+var num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("{0}{0}{0}", num);
+Console.WriteLine("{0} {0}", num);
+Console.WriteLine("{0} {0}", num);
+Console.WriteLine("{0} {0}", num);
+Console.WriteLine("{0}{0}{0}", num);
---
+++ Homework_Operants/HwEx13/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter the amount of Celsius: ");
+var temp = Convert.ToInt32(Console.ReadLine());
+
+var kelvin = temp + 273;
+var faren = ((temp * 9) / 5) + 32;
+
+Console.WriteLine($"Kelvin = {kelvin}");
+Console.WriteLine($"Fahrenheit = {faren}");
